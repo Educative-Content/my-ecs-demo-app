543a571d78ae491ebe33c808b6bb14d30f4bab2a
sohaibbinmohsin <62615453+sohaibbinmohsin@users.noreply.github.com>
2023-05-02 15:23:42 +0500

    first changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './ecs.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>
+            Congratulations 🎉
+          </b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+        Your React application is successfully running on <b>Amazon ECS</b> 🚀🔥
+        </p>
       </header>
     </div>
   );
